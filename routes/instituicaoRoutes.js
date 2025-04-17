184f2784d808d1dd1cb4548ebc820294aea837ce
danielhennig <danielhennig10@gmail.com>
2025-04-16 21:15:26 -0300

    refactor: ajustar formatação e organização do código em instituicaoRoutes

--- routes/instituicaoRoutes.js
+++ routes/instituicaoRoutes.js
@@ -23,16 +23,32 @@ router.post('/', async (req, res) => {
   }
 });
 router.get('/', async (req, res) => {
-    try {
-      const instituicoes = await Instituicao.findAll({
-        attributes: ['id', 'nome'],
-        order: [['id', 'ASC']]
-      });
-  
-      res.status(200).json(instituicoes);
-    } catch (error) {
-      res.status(500).json({ erro: error.message });
+  try {
+    const instituicoes = await Instituicao.findAll({
+      attributes: ['id', 'nome'],
+      order: [['id', 'ASC']]
+    });
+
+    res.status(200).json(instituicoes);
+  } catch (error) {
+    res.status(500).json({ erro: error.message });
+  }
+});
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const instituicao = await Instituicao.findByPk(id);
+    if (!instituicao) {
+      return res.status(404).json({ mensagem: "Instituição não encontrada" });
     }
-  });
+
+    await instituicao.destroy(); // contas associadas serão removidas via CASCADE
+
+    res.status(200).json({ mensagem: "Instituição e contas associadas foram deletadas com sucesso" });
+  } catch (error) {
+    res.status(500).json({ erro: error.message });
+  }
+});
 
 module.exports = router;
